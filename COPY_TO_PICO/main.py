d670bc214657bbcf6a5cb8ff7cb6b26ab2170c7d
Grant Peterson <dgrantpete@gmail.com>
2025-11-19 15:17:23 -0700

    Delete old project files

--- COPY_TO_PICO/main.py
+++
@@ -1,9 +0,0 @@
-import machine
-from utime import sleep
-
-if machine.reset_cause() is machine.PWRON_RESET:
-    sleep(3)
-    import display
-    
-else:
-    print("Crash reset detected, will not run file automatically.")
