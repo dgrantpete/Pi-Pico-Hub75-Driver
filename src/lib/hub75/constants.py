286f64e08a7b171dbd3804acf8cf89e3200b4335
Grant Peterson <dgrantpete@gmail.com>
2025-11-26 20:43:47 -0700

    Reorganize ARCH to live in constants

--- src/lib/hub75/constants.py
+++ src/lib/hub75/constants.py
@@ -1,1 +1,23 @@
+import sys
+
 COLOR_BIT_DEPTH = 8
+
+if sys.implementation.name == 'micropython':
+    
+    ARCHES = [
+        None, 
+        'x86', 
+        'x64', 
+        'armv6', 
+        'armv6m', # RP2040
+        'armv7m', 
+        'armv7em', 
+        'armv7emsp', # RP2350
+        'armv7emdp',
+        'xtensa', 
+        'xtensawin', 
+        'rv32imc', 
+        'rv64imc'
+    ]
+    
+    ARCH = ARCHES[(sys.implementation._mpy >> 10) & 0b1111]
--- src/lib/hub75/native/__init__.py
+++ src/lib/hub75/native/__init__.py
@@ -1,31 +1,10 @@
-def _get_arch():
-    import sys
+from lib.hub75.constants import ARCH
 
-    ARCHES = [
-        None, 
-        'x86', 
-        'x64', 
-        'armv6', 
-        'armv6m', # RP2040
-        'armv7m', 
-        'armv7em', 
-        'armv7emsp', # RP2350
-        'armv7emdp',
-        'xtensa', 
-        'xtensawin', 
-        'rv32imc', 
-        'rv64imc'
-    ]
-
-    return ARCHES[(sys.implementation._mpy >> 10) & 0b1111]
-
-_arch = _get_arch()
-
-if _arch == 'armv6m':
+if ARCH == 'armv6m':
     from lib.hub75.native.armv6m import load_rgb888, load_rgb565 # type: ignore
-elif _arch == 'armv7emsp':
+elif ARCH == 'armv7emsp':
     from lib.hub75.native.armv7emsp import load_rgb888, load_rgb565 # type: ignore
 else:
-    raise ImportError(f"hub75.native is not supported on architecture: {_arch!r}")
+    raise ImportError(f"hub75.native is not supported on architecture: {ARCH!r}")
 
 __all__ = ['load_rgb888', 'load_rgb565']
